e23dfb653a4b53f0086a986cc810046931aceced
James Benjamin <james.abenj@gmail.com>
2020-05-19 17:16:04 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -15,7 +15,7 @@ class SchoolClassesController < ApplicationController
   end
 
   def update
-    @school_class = SchoolClass.find(params[:id]
+    @schoolclass = SchoolClass.find(params[:id]
     @school_class.update(school_class_params)
     redirect_to school_class_path
   end
