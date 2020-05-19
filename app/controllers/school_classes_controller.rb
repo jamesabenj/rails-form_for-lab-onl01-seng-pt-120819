b5fed9e019188fc78bd4ef795d76f48d021a09c0
James Benjamin <james.abenj@gmail.com>
2020-05-19 17:01:58 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -3,14 +3,27 @@ class SchoolClassesController < ApplicationController
   end
 
   def create
+    Student.create(student_params)
+    redirect_to student_path
   end
 
   def show
+    @student = Student.find(params[:id])
   end
 
   def edit
+    @student = Student.find(params[:id])
   end
 
   def update
+    @student = Student.find(params[:id])
+    @student.update(student_params)
+    redirect_to student_path
   end
+
+  private
+
+    def student_params
+      params.require(:school_class).permit(:title, :room_number)
+    end
 end
