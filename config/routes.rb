e0242dd6ea7828eb26b6804dd07bee9bb21ba9f5
James Benjamin <james.abenj@gmail.com>
2020-05-19 18:00:24 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/students/:id', to: 'students#show'
   patch 'students/:id', to: 'students#update'
 
-  resources 'school_classes', only: [:new, :create, :edit, :update]
+  resources 'school_classes', only: [:new :create, :edit, :update]
   get 'school_classes/:id', to: 'school_classes#show'
   patch 'school_classes/:id', to: 'school_classes#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
