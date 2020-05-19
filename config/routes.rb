8dc1b3763e95e756dd2b604ede67944690cf9fe0
James Benjamin <james.abenj@gmail.com>
2020-05-19 17:06:40 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources 'students', only: [:new, :create, :edit, :update,]
   get '/students/:id', to: 'students#show'
+
+  resources 'school_classes', only: [:new, :create, :edit, :update,]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
